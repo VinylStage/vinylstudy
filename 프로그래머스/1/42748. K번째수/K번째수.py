8e8971291d40fa51eacbb7b0f02097c19dc90827
VinylStage <mcpe9869@gmail.com>
2023-10-18 10:33:59 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,2 @@
 def solution(array, commands):
-    answer = []
-    # [i-1:j], 정렬하고 [k-1]
-    for a in commands:
-        answer.append(sorted(array[a[0]-1:a[1]])[a[2]-1])
-    return answer
+    return [sorted(array[a[0]-1:a[1]])[a[2]-1] for a in commands]
