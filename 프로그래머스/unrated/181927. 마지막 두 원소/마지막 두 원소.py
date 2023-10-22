87b539560a7140b9415def7caf6f98cfc7124e34
VinylStage <mcpe9869@gmail.com>
2023-10-22 16:34:28 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -3,6 +3,6 @@ def solution(num_list):
     second = num_list[-2:-1][0]
     if last > second:
         num_list.append(last - second)
-    elif last <= second:
+    else:
         num_list.append(last*2)
     return num_list
