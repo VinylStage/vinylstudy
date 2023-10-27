d69a79d99be296509f17766e1e6045ce9bfa45ae
VinylStage <mcpe9869@gmail.com>
2023-10-27 22:05:27 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,8 @@
+select order_id, product_id, date_format(out_date, '%Y-%m-%d') out_date,
+case
+    when OUT_DATE <= '2022-05-01' then '출고완료'
+    when OUT_DATE is null then '출고미정'
+    else '출고대기'
+end 출고여부
+from food_order
+order by order_id
