1efeb590fab0044f117f9be77bd597d2034a46fe
VinylStage <mcpe9869@gmail.com>
2023-10-27 22:13:11 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,6 +1,6 @@
 select concat('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) file_path
 from used_goods_board a
-left join used_goods_file b
+join used_goods_file b
 using (board_id)
 where views = (select max(views) from used_goods_board)
 order by views desc, file_id desc
